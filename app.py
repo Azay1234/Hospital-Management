d1c2c1fd95478702695b58b0fb81bd3a26f5138d
Azay1234 <b.azrv2012@gmail.com>
2025-04-29 10:48:11 -0400

    Fix: Set correct port for Render

--- app.py
+++ app.py
@@ -565,4 +565,4 @@ def add_medical_record():
 
 # ========== Run Server ==========
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5050)
+    app.run(debug=True, host='0.0.0.0', port=10000)
